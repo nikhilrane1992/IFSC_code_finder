95d97f23319f4060ab2729658df5ed83dff4a495
Nikhil Rane <nikhilrane1992@gamil.com>
2016-02-26 21:43:38 +0530

    added static files

---
+++ IFSC_code_finder/static/js/custom.js
@@ -0,0 +1,113 @@
+$(document).ready(function() {
+    var listClass='list-group-item';    
+    $('#list').click(function(event){
+        event.preventDefault();
+        $('#products .item').addClass(listClass);    
+    });
+    $('#grid').click(function(event){
+        event.preventDefault();
+        $('#products .item')
+            .removeClass(listClass)
+            .addClass('grid-group-item');        
+    });
+});
+
+angular.module("movieApp", ['ui.bootstrap', 'angular-loading-bar'])
+.config(['cfpLoadingBarProvider', function(cfpLoadingBarProvider) {
+    cfpLoadingBarProvider.includeSpinner = true;
+  }])
+.controller("movieCtrl", function($scope,$http){
+
+    $scope.selectedFilterList = []
+
+    $scope.recordNotFound = '';
+
+    $scope.maxSize = 5;
+    $scope.bigTotalItems = 0;
+    $scope.bigCurrentPage = 1;
+    $scope.itemPerPage = 10;
+
+    var getMovieList = function(){
+        $http.post('/movies_by_filter/',{pageNo: $scope.bigCurrentPage, itemPerPage: $scope.itemPerPage, selectedFilterList: $scope.selectedFilterList}).
+            success(function(data, status, headers, config) {
+                if (data.status){
+                    console.log(data);
+                    $scope.movieList = data.movieList;
+                    $scope.bigTotalItems = data.totalEntries;
+                    $scope.bigCurrentPage = data.pageNo;
+                    console.log('Big Current Page: ' + $scope.bigCurrentPage);
+                    console.log('Big Total Items: ' + $scope.bigTotalItems);
+                    console.log('Max Size: ' + $scope.maxSize);
+                    $scope.recordNotFound = ''
+                }else{
+                    $scope.recordNotFound = data.validation;
+                    $scope.movieList = []
+                }
+            }).
+            error(function(data, status, headers, config) {
+                console.log(data);
+            });
+    }
+
+
+    $scope.pageChanged = function(page) {
+        console.log('Page: ' + page);
+        console.log('Big Current Page: ' + $scope.bigCurrentPage);
+        console.log('Big Total Items: ' + $scope.bigTotalItems);
+        console.log('Max Size: ' + $scope.maxSize);
+        $scope.bigCurrentPage = page;
+        getMovieList();
+    };
+
+    var searchFilter = function(){
+        $http({method: 'GET', url: '/filter/list/'})
+            .then(function successCallback(response) {
+                $scope.genreList = response.data.genreList;
+                $scope.languageList = response.data.languageList;
+                $scope.sortByList = response.data.sortByList;
+            }, function errorCallback(response) {
+               
+            });
+    }
+
+    var init = function(){
+        searchFilter();
+        getMovieList();
+    }
+    init();
+
+    $scope.clearFilter = function(){
+        for(var i=0; i < $scope.genreList.length; i++){
+            $scope.genreList[i].isSelected = false;
+        }
+        for(var i=0; i < $scope.languageList.length; i++){
+            $scope.languageList[i].isSelected = false;
+        }
+        for(var i=0; i < $scope.sortByList.length; i++){
+            $scope.sortByList[i].isSelected = false;
+        }
+        $scope.selectedFilterList = []
+        getMovieList();
+    }
+
+    $scope.clickFilter = function(obj){
+        $scope.bigCurrentPage = 1;
+        obj.isSelected=!obj.isSelected;
+        console.log(obj.isSelected)
+        if (obj.isSelected){
+            $scope.selectedFilterList.push(obj);
+            console.log($scope.selectedFilterList)
+            getMovieList();
+        }else{
+            var index = $scope.selectedFilterList.indexOf(obj);
+            $scope.selectedFilterList.splice(index, 1);     
+            // $scope.selectedFilterList.pop(obj);
+            console.log($scope.selectedFilterList)
+            getMovieList();
+        }
+    }
+        
+
+
+});
+
